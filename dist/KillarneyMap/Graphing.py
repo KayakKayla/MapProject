1bf30ada3bf38149451b84785b1f16751e495a9f
KayakKayla <137660752+KayakKayla@users.noreply.github.com>
2023-09-10 01:03:49 -0400

    Initial commit

---
+++ dist/KillarneyMap/Graphing.py
@@ -0,0 +1,120 @@
+from PIL import Image, ImageDraw
+import re
+class Graph:
+    def __init__(self):  # add a dictionary with all the nodes
+        # has name and location of node
+        self.nodes = {}
+        self.locations = {}
+        self.types = {"Classroom": [],
+                      "Hallway": [],
+                      "Stairs": [],
+                      "Special": [],
+                      "Elevator": []}
+        
+    def importNodes(self, csvFile):
+        f = open(csvFile, "r")
+        keys = {}
+        list = []
+        for line in f:
+            information = line.split(",")
+            name = information[0]
+            nodeType = information[1]
+            if nodeType == "Classroom":
+                alternativeName = re.sub(r"[\n\t\s]*", "", information[5])
+                if alternativeName != "":
+                    keys[alternativeName] = name
+                    list.append(alternativeName)
+                else:
+                    keys[name] = name
+                    list.append(name)
+            CoOrdinates = [int(x) for x in information[2].split()]
+            self.locations[name] = CoOrdinates
+            connectionNames = information[3].split()
+            connectionDistances = [int(x) for x in information[4].split()]
+            self.addNode(name, nodeType, connectionNames, connectionDistances)
+        return list, keys
+
+    def addNode(self, name, type="Classroom", connections=["Z"], distances=[0]):
+        self.nodes[name] = Node(name, type)
+        self.types[type].append(name)
+
+        # self.types[type].append(name)  # figure out if you should use name or actual node
+        for i in range(len(connections)):
+            self.nodes[name].addConnection(connections[i], distances[i])
+
+    def display(self):
+        for key in self.nodes:
+            print(f"{self.nodes[key].data()}")
+
+    def classExists(self, classroom):
+        if classroom in self.nodes:
+            return True
+        return False
+
+    def displayPath(self, startPoint, endPoint):
+        result = self.findPath(startPoint, endPoint)
+        image = Image.open('FullFloor.png')
+        draw = ImageDraw.Draw(image)
+        for i in range(len(result) - 1):
+            if result[i] != "B1Aud" and result[i+1] != "B1Aud":
+                if self.nodes[result[i]].type == "Stairs" and self.nodes[result[i+1]].type == "Stairs":
+                    pass
+                else:
+                    draw.line(self.locations[result[i]] + self.locations[result[i + 1]], fill=100, width=2)
+        image.save(f"images/{startPoint}_{endPoint}.png")
+
+    def findPath(self, startPoint, endPoint):
+        # while end not found?
+        # Turn off all classroom nodes that aren't the one
+        beginning = self.nodes[startPoint].name
+        start = self.nodes[startPoint]
+        end = self.nodes[endPoint]
+        extra = []
+        if "Aud" in startPoint or "Aud" in endPoint:
+            extra += ["hB19", "hB16", "B2Aud", "B2Aud1"]
+        if startPoint == "C1Caf" or endPoint == "C1Caf":
+            extra += ["hC2","hC3"]
+        distance = dict.fromkeys(self.types["Hallway"] + self.types["Stairs"] + [startPoint, endPoint, "A1Lib"]+extra,
+                                 [9999999999, "none"])  # set all distances to infinity, also optimized to not include other classrooms
+        unexplored = dict.fromkeys(distance, 9999999999)  # there has to be a better w ay
+        distance[start.name] = [0, start.name]
+        # unexplored.pop(start.name)
+        # minUnexplored = min(unexplored, key=unexplored.get)
+        # start = self.nodes[minUnexplored]  # then repeat
+        # print(start.name)
+        while start.name != end.name:
+            # print(start.name)
+            for room in start.connections:
+                # start.connections[room]+distance[start.name][0] adds distance from previous point to current point
+                # improve it later so that it has no stuff
+                if room in unexplored:
+                    # print(room)
+                    if start.connections[room] + distance[start.name][0] < distance[room][0]:
+                        distance[room] = [start.connections[room] + distance[start.name][0], start.name]
+                        unexplored[room] = start.connections[room] + distance[start.name][0]
+            unexplored.pop(start.name)  # removes the point that has already been explored
+            minUnexplored = min(unexplored, key=unexplored.get)  # finds the minimum pont that hasn't been explored yet
+            start = self.nodes[minUnexplored]  # finds the new starting point
+        # print("------------------")
+        # print(distance)
+        point = end.name
+        path = []
+        while point != beginning:
+            path.insert(0, point)
+            # print(point, distance[point])
+            point = distance[point][1]
+        path.insert(0, beginning)
+        return path
+
+
+class Node:
+    def __init__(self, name, type="classroom"):
+        self.name = name
+        self.type = type  # classroom, hallway, stairs, elevator
+        self.connections = {}
+
+    def addConnection(self, nodeName, distance):
+        self.connections[nodeName] = distance
+
+    def data(self):
+        return f"{self.name}: {self.type}, {self.connections}"
---
+++ main.py
@@ -0,0 +1,132 @@
+import kivy.factory
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from Graphing import *
+from kivy.config import Config
+from kivy.uix.button import Button
+from kivy.properties import ListProperty
+from kivy.core.text import LabelBase
+from kivy.uix.spinner import SpinnerOption
+
+
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
+
+# from kivy.uix.textinput import TextInput
+class MySpinnerOption(SpinnerOption):
+    pass
+
+class RoundButton(Button):
+    btn_color = ListProperty()
+
+
+class MainScreen(BoxLayout):
+    Map = Graph()
+    whatList, connector = Map.importNodes("finalmap.csv")
+
+    def test(self):
+        print("yes someone is typing")
+        print(self.ids)
+
+    def searchValues(self, searchValue, spinnerId):  # add value and place
+        list = [x for x in self.whatList if searchValue.lower() in x.lower()]
+        if len(list) == 0:
+            self.ids[spinnerId].text = "no result"
+        else:
+            self.ids[spinnerId].text = list[0]
+        self.ids[spinnerId].values = list
+
+    def changeValue(self, value, place):
+        self.ids[place].text = value
+        # place = value
+
+    def drawPath(self):
+        if self.ids.starting_spinner.text != "no result" and self.ids.destination_spinner.text != "no result":
+            start = self.connector[self.ids.starting_spinner.text]
+            end = self.connector[self.ids.destination_spinner.text]
+            self.Map.displayPath(start, end)
+            self.ids.map_id.source = f"images/{start}_{end}.png"
+
+    def clear(self):
+        self.ids.destination_input.text = ""
+        self.ids.starting_input.text = ""
+        self.ids.destination_spinner.values = self.whatList
+        self.ids.starting_spinner.values = self.whatList
+        self.ids.map_id.source = "FullFloor.png"
+
+    def select(self):
+        print(self.ids.drop.text)
+
+    pass
+
+
+# [x for x in list if "input" in x]
+class ClassroomInput(BoxLayout):
+    pass
+
+
+class KillarneyMapApp(App):
+    def build(self):
+        self.icon = "paw.png"
+        return MainScreen()
+
+
+if __name__ == '__main__':
+    LabelBase.register(name='Varela', fn_regular='VarelaRound-Regular.ttf')
+    KillarneyMapApp().run()
+
+# from kivy.base import runTouchApp
+# from kivy.lang import Builder
+# from kivy.factory import Factory
+#
+# Builder.load_string('''
+# <FDDButton@Button>:
+#     size_hint_y: None
+#     height: '50dp'
+#
+# <FilterDD>:
+#     auto_dismiss: False
+# ''')
+#
+#
+# class FilterDD(Factory.DropDown):
+#     def __init__(self, buttons, **kwargs):
+#         super(FilterDD, self).__init__(**kwargs)
+#         self._buttons = buttons
+#         self._filter = filter = Factory.TextInput(size_hint_y=None)
+#         self.add_widget(filter)
+#         filter.bind(text=self.apply_filter)
+#         self.apply_filter(None, '')
+#
+#         # You need this to add widgets here for apply_filter2:
+#         # for text in buttons:
+#         #    self.add_widget(Factory.FDDButton(text=text))
+#
+#     def apply_filter(self, wid, value):
+#         self.clear_widgets()
+#         self.add_widget(self._filter)
+#         for btn in self._buttons:
+#             if not value or value in btn:
+#                 self.add_widget(Factory.FDDButton(text=btn))
+#
+#     def apply_filter2(self, wid, value):
+#         if not value:
+#             return
+#         for btn in self.container.children[:-1]:  # skip textinput
+#             if value in btn.text:
+#                 self.scroll_to(btn)
+#                 break
+#
+#
+# fdd = FilterDD(["one", "two", "three", "four", "five", "six",
+#                 "seven", "eight", "nine", "ten"])
+#
+# runTouchApp(Builder.load_string('''
+# #:import fdd __main__.fdd
+#
+# Button:
+#     size_hint: None, None
+#     pos: 200, 200
+#     on_press:
+#         fdd.open(self)
+# '''))
